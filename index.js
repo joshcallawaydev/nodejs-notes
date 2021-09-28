2d9c5f925639185f2d77aad5305813947341ddf0
joshcallawaydev <joshrcal89@gmail.com>
2021-09-28 19:04:55 +0000

    playing with _.pluck

--- index.js
+++ index.js
@@ -3,3 +3,20 @@ var _ = require('underscore');
 var results = _.contains([1, 2, 3], 3)
 
 console.log(results)
+
+var people = [{
+    name: 'Josh',
+    age: 32
+}, {
+    name: 'Nat',
+    age: 27
+}, {
+    name: 'Zara',
+    age: 3
+}, {
+    name: 'Roy',
+    age: 79
+}]
+
+var pluck = _.pluck(people, 'age');
+console.log(pluck)
